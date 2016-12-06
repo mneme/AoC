871e01eaaebcd3fa83edf9a019dfcd1581a77e3e
David Olsson <david.olsson@softhouse.se>
2016-12-06 21:04:45 +0100

    and some more format fixes.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ day2 = require('./day2/day2.js');
 day3 = require('./day3/day3.js');
 
 function d1(){
-	const instructions  = require('fs').readFileSync('day1/day1.input', 'utf8').split(", ");
-	console.log('day1', day1.distance(day1.apply(...instructions)));
-	console.log('day1', day1.distance(day1.twice(...instructions)));
+  const instructions  = require('fs').readFileSync('day1/day1.input', 'utf8').split(", ");
+  console.log('day1', day1.distance(day1.apply(...instructions)));
+  console.log('day1', day1.distance(day1.twice(...instructions)));
 }
 
 function d2(){
@@ -19,7 +19,6 @@ function d3(){
   console.log('day3.1', day3.apply(instructions));
   console.log('day3.2', day3.apply2(instructions));
 }
-
 
 d1();
 d2();
