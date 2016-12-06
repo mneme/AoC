556980fc91a729b321e9d61583027cb34088584c
David Olsson <david.olsson@softhouse.se>
2016-12-06 21:09:34 +0100

    scaffold for day4.

--- day3/day3.spec.js
+++ day3/day3.spec.js
@@ -4,7 +4,7 @@ var chai = require('chai'),
     should = chai.should(),
     day3 = require('./day3.js');
 
-describe('day2', function(){
+describe('day3', function(){
   describe('apply', function () {
     it('should get the right amount of valid triangles', function(){
       day3.apply("5 10 25").should.equal(0);
---
+++ day4/day4.js
@@ -0,0 +1,1 @@
+const R = require('ramda');
