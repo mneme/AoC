7ddc43bee81cbbcda2211ef22ec17d7d611f1e6a
David Olsson <david.olsson@softhouse.se>
2016-12-07 01:32:55 +0100

    day4 banged out. Tests still needed.

--- day4/day4.js
+++ day4/day4.js
@@ -1,1 +1,57 @@
+'use strict'
 const R = require('ramda');
+
+
+const groupEncryptedName = R.compose(R.toPairs,R.countBy(R.identity)),
+      parse = R.compose(R.map(parseRow),R.split('\n'));
+
+function parseRow(row){
+  const matches = row.match(/(.+)-(\d*)\[(.+)\]/);
+  return [matches[1], parseInt(matches[2]), matches[3]];
+}
+
+function checksum(name){
+  return R.compose(
+    R.join(''),
+    R.map(R.head),
+    R.take(5),
+    R.sort((a, b) => {
+      if (a[1] === b[1])
+        return a[0] < b[0] ? -1 : 1;
+      return b[1] - a[1];
+    }), 
+    groupEncryptedName,
+    R.replace(/-/g, '')
+  )(name);
+}
+
+function shift(char, n){
+  return String.fromCharCode((char.charCodeAt(0) - 97 + n) % 26 + 97)
+}
+
+function valid(row){
+  return row[2] === checksum(row[0])
+}
+
+function decrypt(row){
+  return [R.map((char) => char === '-' ? ' ' : shift(char,row[1]),row[0].split('')).join(''), row[1], row[2]];
+}
+
+exports.apply = function(input){
+  return R.compose(
+    R.sum,
+    R.map(R.nth(1)),
+    R.filter(R.curry(valid)),
+    parse
+  )(input)
+}
+
+exports.apply2= function(input, find){
+  return R.compose(
+    R.nth(1),
+    R.find((row) => row[0].match(find)),
+    R.map(decrypt),
+    R.filter(R.curry(valid)),
+    parse
+  )(input);
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 day1 = require('./day1/day1.js');
 day2 = require('./day2/day2.js');
 day3 = require('./day3/day3.js');
+day4 = require('./day4/day4.js');
 
 function d1(){
   const instructions  = require('fs').readFileSync('day1/day1.input', 'utf8').split(", ");
@@ -20,6 +21,13 @@ function d3(){
   console.log('day3.2', day3.apply2(instructions));
 }
 
+function d4(){
+  const instructions = require('fs').readFileSync('day4/day4.input', 'utf8');
+  console.log('day4.1', day4.apply(instructions));
+  console.log('day4.2', day4.apply2(instructions, 'north'));
+}
+
 d1();
 d2();
 d3();
+d4();
