13ea5511b9d063702d16a0382e4b89ddaafa28b4
David Olsson <david.olsson@softhouse.se>
2016-12-10 08:47:02 +0100

    cleanup of day7

--- day7/day7.js
+++ day7/day7.js
@@ -1,10 +1,6 @@
 R = require('ramda');
 
-const log = R.bind(console.log, console);
-
-let str = "ioxxoj[asdfgh]zxcvbn";
 let parens = /\[.*?\]/g;
-
 
 function aperture(n){
   return R.compose(
@@ -23,15 +19,6 @@ const hasAbbaCode = R.both(
     R.compose(R.any(isAbbaCode), R.split(parens))
   );
 
-function countAbba(input){
-  return R.compose(
-    R.length,
-    R.filter(hasAbbaCode),
-    R.split('\n')
-  )(input);
-}
-
-
 function hasAbaCode(str){
   const potential = R.compose(
     R.filter(isAbaCode),
@@ -42,31 +29,26 @@ function hasAbaCode(str){
   const aba = potential(str.split(parens)),
         bab = potential(str.match(parens));
 
-
-  return R.length(R.intersectionWith(matchingBab,aba,bab));
+  return R.length(R.intersectionWith((a, b) =>{
+      return [a[1],a[0],a[1]].join('') === b.join('')
+    }, aba, bab));
 }
 
 function isAbaCode(arr){
   return (arr[0] === arr[2] && arr[0] !== arr[1]);
 }
 
-function matchingBab(aba, bab){
-  return [aba[1],aba[0],aba[1]].join('') === bab.join('')
-}
-
-const instructions = require('fs').readFileSync('./day7/day7.input', 'utf8');
-
-function countAba(input){
+function count(code, input){
   return R.compose(
     R.length,
-    R.filter(hasAbaCode),
+    R.filter(code),
     R.split('\n')
   )(input);
 }
 
-console.log('day7.1: ', countAbba(instructions));
-console.log('day7.2: ',countAba(instructions));
+const instructions = require('fs').readFileSync('./day7/day7.input', 'utf8');
 
-
+console.log('day7.1: ', count(hasAbbaCode, instructions));
+console.log('day7.2: ',count(hasAbaCode, instructions));
 
 
